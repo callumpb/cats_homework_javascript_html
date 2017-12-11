22e5821452de5a8a9dab9cd1720328623dcf1877
Callum <callumpb@hotmail.com>
2017-12-11 17:20:08 +0000

    ext 1 added

--- public/app.js
+++ public/app.js
@@ -1,4 +1,27 @@
+const cats = [
+  {
+  name: 'Boba',
+  favouriteFood: 'Sock fluff',
+  image: 'http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg'
+},
+  {
+  name: 'Barnaby',
+  favouriteFood: 'Tuna',
+  image: 'https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg'
+},
+  {
+  name: 'Max',
+  favouriteFood: 'Whiskas Temptations',
+  image: 'http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg'
+  }
+]
+
+
 const app = function () {
+  for(let cat of cats) {
+    createCat(cat.image, cat.name, cat.favouriteFood)
+  }
+
 
   // const ul = document.createElement('ul');
   // ul.classList.add('cat');
@@ -24,7 +47,7 @@ const app = function () {
   // cats.appendChild(ul);
   const image = "https://ih1.redbubble.net/image.22839457.9845/flat,1000x1000,075,f.u3.jpg"
 createCat(image, 'Hamish the Danish', 'Danish')
-}
+};
 
 const createUl = function() {
   const ul = document.createElement('ul');
